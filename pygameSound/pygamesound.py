72cc4ab110799bad5bf9865cb848c4929ee4655a
Igor Daniel <72262654+IgorDanielS@users.noreply.github.com>
2024-01-30 20:34:47 -0300

    Comentários adicionados pygamesound.py

--- pygameSound/pygamesound.py
+++ pygameSound/pygamesound.py
@@ -1,3 +1,5 @@
+#Desafio: Rodar alguma música utilizando o python.
+
 import pygame
 
 pygame.init()
@@ -5,5 +7,8 @@ file = 'LegiãoUrbana.mp3'
 pygame.mixer.music.load(file)
 pygame.mixer.music.play()
 
+#Utilizei esse input de forma intuitiva para que o código não pare antes da música acabar, caso contrário o código para antes da música finalizar
+#Outra alternativa seria usar o time.sleep(), mas aí teria que calcular o tamanho da música e colocar como parâmetro, nesse sentido, fica mais 
+#fácil utilizar esse método do inputEspera
 inputEspera = input('Tocando a musica...')
 
