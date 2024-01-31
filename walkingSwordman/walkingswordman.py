4b5d8903d40d2b59774978fab710f6631fcbd093
Igor Daniel <igordaniel1903@gmail.com>
2024-01-31 14:23:46 -0300

    Mini jogo de terminal que fiz para passar o tempo e colocar alguns conceitos em prática

---
+++ walkingSwordman/walkingswordman.py
@@ -0,0 +1,32 @@
+import os
+
+player = {"nome": "Python","x": 0, "y":0}
+
+def andar(direcao):
+    if direcao == "d":
+        player['x'] += 1
+    elif direcao == "a":
+        player['x'] -= 1
+    elif direcao == "w":
+        player['y'] -= 1
+    elif direcao == "s":
+        player['y'] += 1
+
+while True:
+    os.system('cls')
+
+    print("----------------")
+    for y in range(5):
+        print("\n")
+        for x in range(10):
+            if player['x'] == x and player['y'] == y:
+                print("🤺", end="")
+            else:
+                print("🟩", end="")
+                
+    print("\n----------------")
+
+    direcao = input("Próxima direção (w,s,d,a) \n('end' para finalizar): ")
+    if direcao == 'end':
+        break
+    andar(direcao)
